cf4e9e47a7cdf8bf3cd291e1c9acebb324f4b483
1jack80 <work.davidmainoo@gmail.com>
2023-04-05 22:36:52 +0000

    Add file receiver functions

--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
-import (
-	"swift/receiver"
-	"swift/sender"
-	"swift/utils"
-	"sync"
-)
-
 func main() {
-	role := utils.WelcomeScreen()
-	var wg sync.WaitGroup
+	// receive file
 
-	serverPort := 1234
-	if role == 1 {
-		sender.StartSender(&wg, serverPort)
-	} else if role == 2 {
-		receiver.StartReceiver(&wg)
-	}
-	wg.Wait()
+	// decrypt file
+
+	// save file
 }
