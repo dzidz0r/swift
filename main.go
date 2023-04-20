ff35f1ff422f28be9304b7382b53137bddd3ddcc
1jack80 <work.davidmainoo@gmail.com>
2023-04-20 19:26:01 +0000

    main function

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"sync"
+	"time"
+
+	"github.com/321swift/swift/client"
+	"github.com/321swift/swift/server"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+
+	file := "./go.mod"
+	cli := client.NewClient()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		time.Sleep(time.Second * 2)
+		cli.Connect(":5050")
+
+		// time.AfterFunc(time.Second*2, func() {
+		// cli.Send(file)
+		// })
+		cli.Receive()
+
+	}()
+	//
+	srvr := server.NewServer()
+	srvr.Start()
+	srvr.Receive()
+
+	wg.Wait()
+}
