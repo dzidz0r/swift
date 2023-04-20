cfd59700c07ff2bb9cb0e2ae8d5764e55afb2801
1jack80 <work.davidmainoo@gmail.com>
2023-04-20 19:25:20 +0000

    Add client type and methods

---
+++ client/client.go
@@ -0,0 +1,82 @@
+package client
+
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+type client struct {
+	conn net.Conn
+	Conn net.Conn
+}
+
+func NewClient() *client {
+	return &client{}
+}
+
+func (c *client) Connect(address string) {
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.Conn = conn
+	c.conn = conn
+}
+
+func (c *client) Send(filePath string) error {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// send file size
+	err = binary.Write(c.conn, binary.LittleEndian, int64(len(data)))
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// send data
+	i, err := io.CopyN(c.conn, bytes.NewReader(data), int64(len(data)))
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	log.Printf("File sent successfully: %d / %d bytes written", i, len(data))
+
+	return nil
+}
+
+func (c *client) Receive() {
+
+	// receive file size
+	var dataSize int64
+	err := binary.Read(c.conn, binary.LittleEndian, &dataSize)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// recieve data
+	data := new(bytes.Buffer)
+	i, err := io.CopyN(data, c.conn, dataSize)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Received %d bytes from connection", i)
+
+}
+
+func (c *client) Disconnect() error {
+	err := c.conn.Close()
+	if err != nil {
+		log.Println(err)
+	}
+	return err
+}
